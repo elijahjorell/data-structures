1746aa8134389c558c4da0e0e5be4de221a22aee
elija <elijahesmero@gmail.com>
2020-08-28 18:48:28 +1000

    Add runtime tools

---
+++ tools.js
@@ -0,0 +1,20 @@
+const performance = require('perf_hooks').performance;
+
+module.exports = {
+    getRuntime: function(fn) {
+        const tries = 30;
+    
+        var startTime;
+        var finishTime;
+        var times = [];
+        
+        for (var i = 0; i < tries; i++) {
+            startTime = performance.now();
+            fn;
+            finishTime = performance.now();
+            times.push(finishTime - startTime);
+        }
+    
+        console.log(`Time elapsed: ${ times.reduce((acc, val) => acc + val) / tries } milliseconds`)
+    }
+}
