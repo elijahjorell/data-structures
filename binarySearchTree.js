a95a328394dc9cc531f7cc2bf508de66b314be73
elijahjorell <elijahesmero@gmail.com>
2020-08-28 14:01:05 +1000

    Create stack class

---
+++ binarySearchTree.js
@@ -0,0 +1,25 @@
+class BinarySearchTree {
+    constructor() {
+        
+    }
+    validate(value) {
+        
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = undefined;
+        this.right = undefined;
+    }
+    compare(value) {
+
+    }
+}
+
+
+
+const data = new BinarySearchTree();
+
+console.log(data)
